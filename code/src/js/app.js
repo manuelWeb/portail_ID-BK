21fd7a79eec2a20df5ca1561a50986bde6f81492
macfly <manuel@webdesignord.fr>
2017-08-28 16:22:08 +0100

    intégration ok; carousel ok

--- code/src/js/app.js
+++ code/src/js/app.js
@@ -1,1 +1,40 @@
-// Place JS here.
+$(".js-carousel").each(function(){
+  var $carousel = $(this),
+    $carouselContainer = $carousel.find(".js-carousel-container"),
+    $carouselList = $carousel.find(".js-carousel-list"),
+    $carouselItem = $carousel.find(".js-carousel-item"),
+    $carouselMarg = 20,
+    $carouselButton = $carousel.find(".js-carousel-button"),
+    setItemWidth = function(){
+      $carouselList.removeAttr("style");
+      var curWidth = $($carouselItem[0]).outerWidth() * $carouselItem.length + ($carouselItem.length-1) * 20;
+      // console.log(curWidth)
+      $carouselList.css("width", curWidth);
+    },
+    slide = function(){
+      var $button = $(this),
+        dir = $button.data("dir"),
+        curPos = parseInt($carouselList.css("left")) || 0,
+        moveto = 0,
+        containerWidth = $carouselContainer.outerWidth(),
+        listWidth = $carouselList.outerWidth(),
+        before = (curPos + containerWidth),
+        after = listWidth + (curPos - containerWidth);
+        console.log(listWidth)
+      if(dir=="next"){
+        moveto = (after < containerWidth) ? curPos - after : curPos - containerWidth - 20;
+      } else {
+        moveto = (before >= 0) ? 0 : curPos + containerWidth + 20;
+      }
+      
+      $carouselList.animate({
+        left: moveto
+      });
+    };
+  $(window).resize(function(){
+    setItemWidth();
+  });
+  setItemWidth();
+  
+  $carouselButton.on("click", slide);
+})
--- retour.md
+++ retour.md
@@ -20,3 +20,8 @@ Peux tu essayer d’ajouter la couv IDEAS COMFORT 0617 UK en fond
 
 Te rapprocher d’Anissa si besoin d’une sélection produits, sinon voici les bests TL  
 5682018 7573116 9133018 6122311 7483316 1673417 6578017 0626317 9003310 0623314
+
+#### note pour intégration
+- utiliser flexbox dans une moindre mesure.
+- Pas de grid css : browser support too poor.
+- Passer le navigateur en langue étrangère.[s]
